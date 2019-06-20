25a8ad2a365feac7960b3e2919e769e28fe6161a
jundic <jundi_chen@sfu.ca>
2019-06-20 07:21:08 -0700

    It's a test

---
+++ Node.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Node {
+public:
+  T element;
+  Node<T> * next;
+  // friend class LinkedList<T>;
+
+};
+
+//yo i have made new changes to the files
+//oh really
